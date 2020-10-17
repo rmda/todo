ee7cc24f02bda7f69dfcdc63eb655596bdacbc35
rmda <rmda@users.noreply.github.com>
2020-10-16 22:24:17 -0500

    Updates

---
+++ precache-manifest.45631ea7dc3d9f20b859b52bf7bdef64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42de2fa60269b51b1cc79ef80d84e555",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "53444b8fb404d735a0c2",
+    "url": "/todo/static/css/main.852b25d4.chunk.css"
+  },
+  {
+    "revision": "cd23e92c7519bc0616a9",
+    "url": "/todo/static/js/2.7663128a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo/static/js/2.7663128a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53444b8fb404d735a0c2",
+    "url": "/todo/static/js/main.32e608a5.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
